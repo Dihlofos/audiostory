f4636c1accea5926e9e1d3a3168d4aaa3a4da3d1
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-05-23 19:49:45 +0400

    vh hack in js

--- build/js/main.js
+++ build/js/main.js
@@ -32,4 +32,14 @@
     pauseButton.classList.add("is-hidden");
     waves.classList.add("is-hidden");
   }
+
+  setVh();
+
+  window.addEventListener("resize", setVh);
+
+  function setVh() {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
 })();
